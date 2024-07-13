7ba292bb2b7ae2bb7ad49f9da7b97eceaf2dff8a
Amitkupadhyay0 <138156948+Amitkupadhyay0@users.noreply.github.com>
2024-07-14 03:56:48 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kuberastats',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    author='AMIT KUMAR UPADHYAY',
+    description='A library for statistical finance calculations.',
+    url='https://github.com/Amitkupadhyay0/KUBERASTAT',
+    license='AMIT',
+)
